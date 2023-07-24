db3056d50fde870ef1baf5c6388a6eaa87340802
boytimo <boytimo@naver.com>
2023-07-24 17:58:21 +0900

    자바스크립트

---
+++ Conditionals.js
@@ -0,0 +1,10 @@
+console.log("Before Conditional");
+let random = Math.random();
+
+if(random<=0.5)
+{
+    console.log("Your Number Is Less Than 0.5!!");
+    console.log(random);
+}
+
+console.log("After Conditional");
